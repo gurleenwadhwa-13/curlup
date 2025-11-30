1931a5e27802337969e27bd52e0262c38f0e2b93
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-11-30 04:14:58 -0400

    added some changes, mainly lift-state up pattern of react

--- components/ResponsePanel/responsePanel.tsx
+++ components/ResponsePanel/responsePanel.tsx
@@ -1,14 +1,40 @@
-"use client"
 import type { IApiResponse } from "@/types/request.types";
+import { Card, CardContent, CardTitle } from "../ui/card";
+import { Suspense } from "react";
 
-export interface ResponsePanelProps {
-  response: IApiResponse | null
-  error: string | null
+interface ResponsePanelProps {
+  response: {
+    data: any;
+    isLoading: boolean;
+    error?: Error | null;
+  };
 }
 
-export function ResponsePanel({response, error} : ResponsePanelProps) {
-    return (
-        <div className="">
-        </div>
-     );
+export function ResponsePanel({ response }: ResponsePanelProps ) {
+  const { data, isLoading, error} = response
+
+  return (
+      <div className="flex flex-col mx-auto max-w-[500px] overflow-y-auto">
+        <Card className="min-w-[350px] bg-card  border-[#3B82F6]/20 rounded-xl p-3">
+          <CardTitle className="font-bold font-sans text-xl px-1.5">Response</CardTitle>
+          <CardContent className="min-w-[350px] px-1.5">
+            <div>
+
+            </div>
+
+            {/* When Data is being fetched or API response has ben received. */}
+            <div className="container mt-3 bg-gray-100 text-mono text-sm text-black rounded overflow-x-auto">
+              <Suspense fallback={<div>Loading...</div>}>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+              </Suspense>
+            </div>
+
+            {/* Handling Errors */}
+            {error && (
+              <div>There was an error in fetching the data. Kindly check check back later</div>
+            )}
+          </CardContent>
+        </Card>
+      </div>
+    );
 }
