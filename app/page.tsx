7caf41b2edef813cab2728606e1f0518af21ead3
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-11-30 04:13:12 -0400

    added .idea folder to gitignore and added a new component to the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import { QueryPanel } from "@/components/QueryPanel/queryPanel";
-import { ResponsePanel } from "@/components/ResponsePanel/responsePanel"
+import {MainContainer} from "@/components/MainContainer/MainContainer";
 import { Header } from "@/components/Header/Header";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-background flex flex-col">
-      <Header />
-      <main className="flex-1 container mx-auto px-4 py-4">
-        <div className="grid grid-cols-1 lg:grid-cols-[50%_50%] gap-4 h-[calc(100vh-150px)]">
-          <QueryPanel />
-          {/* <ResponsePanel /> */}
-        </div>
-      </main>
-    </div>
-  );
+    return (
+      <div className="min-h-screen bg-background flex flex-col">
+        <Header />
+        <MainContainer />
+      </div>
+    );
 }
